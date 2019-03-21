dd17724d82bdcb74548b3439964ca027d6ae6e09
alex-love <alove2096@gmail.com>
2019-03-20 21:33:33 -0700

    add a basic job list constraint

--- src/components/jobs.js
+++ src/components/jobs.js
@@ -12,14 +12,42 @@ import { rhythm } from "../utils/typography"
 //import custom react components
 import DateRange from "./DateRange"
 
-function Job(props){
-    const {work} = props 
+
+class Job extends React.Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      jobNum: 3,
+    }
+    this.change = this.change.bind(this)
+  }
+
+  change = (event) => {
+    this.setState({jobNum: Number(event.target.value)})
+  }
+
+  render(){
+    const {work} = this.props 
     return (
      <div>
-       {work.map( (job) => {
+       <div>
+         <p>How many jobs to show?</p>
+       <select onChange={this.change} value={this.state.jobNum}>
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+        <option value="6">6</option>
+        <option value="7">7</option>
+        <option value="8">8</option>
+        <option value="9">9</option>
+      </select>
+      </div>
+       {work.slice(0,this.state.jobNum).map( (job) => {
            const {company, position, startDate, endDate, highlights, isCurrentJob} = job;
            return(
-            <div className={props.c}>
+            <div className={this.props.c}>
                 <DateRange startDate={startDate} endDate={endDate} isCurrentJob={isCurrentJob} />
                 <h3>{company}</h3>
                 <p><em>{position}</em></p>
@@ -33,6 +61,7 @@ function Job(props){
        })}
      </div>
     );
+}
 }
 
 class Jobs extends React.Component {
