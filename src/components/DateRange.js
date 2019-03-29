d049b05394c4524c693110faf15e655a6651f2ac
alex love <alove2096@gmail.com>
2019-03-29 12:51:41 -0700

    changes

--- src/components/DateRange.js
+++ src/components/DateRange.js
@@ -9,7 +9,7 @@ function DateRange(props){
     return(
         <div style={{marginBottom: rhythm(-1.8)}}>
             {(isCurrentJob ? 
-             <h4>{startDate}-Present</h4> :
+             <h4>{startDate}-{new Date().getFullYear()}</h4> :
              <h4>{startDate} : {endDate}</h4>
             )}
         </div>
